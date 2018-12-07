9de17ae5253898d079f89aec7e4fbae04a79000e
sonsoleslp <sonsoleslp@hotmail.com>
2018-12-07 14:51:15 +0100

    More options for the future

--- src/App.js
+++ src/App.js
@@ -6,30 +6,49 @@ class App extends Component {
     super(props);
     this.state = {
       left: "",
-      right :""
+      right :"",
+      from: 'xml',
+      to: 'json'
     }
   }
   render() {
     return (
       <div className="App">
-       <header><h1>MoodleXML to JSON</h1></header>
+       <header><h1><i className="material-icons">school</i>  MoodleXML to JSON</h1></header>
        <div className="content"> 
           <div className="content-col left">
-            <h2>From</h2>
+            <div><h2>From</h2>
+            <select onChange={(e)=>{this.setState({from: e.target.value})}} value={this.state.from}>
+              <option value="xml" >MoodleXML</option>
+              <option disabled value="aiken" >Aiken</option>
+              <option disabled value="json" >JSON</option>
+            </select>
+            </div>
             <textarea onChange={(e)=>{this.onWrite(e,'left')}} value={this.state.left}></textarea>
             <div className="buttons">
-              <button onClick={this.convert.bind(this)}>Convert</button>
+              <button onClick={this.convert.bind(this)}>
+                <i className="material-icons">play_arrow</i>Convert</button>
             </div>
           </div>
           <div className="content-col right">
-            <h2>To</h2>
+            <div><h2>To</h2>
+            <select onChange={(e)=>{this.setState({to: e.target.value})}}>
+              <option disabled value="xml" >MoodleXML</option>
+              <option disabled value="aiken" >Aiken</option>
+              <option value="json" >JSON</option>
+            </select>
+            </div>
             <textarea ref="right" onChange={(e)=>{this.onWrite(e,'right')}} value={this.state.right}></textarea>
             <div className="buttons">
               <button onClick={()=>{
                 this.refs.right.select();
                 document.execCommand('copy');
-              }}>Copy</button>
-              <button onClick={()=>{this.download("quiz.json",this.state.right)}}>Download</button>
+              }}>
+                <i className="material-icons">file_copy</i>Copy
+              </button>
+              <button onClick={()=>{this.download("quiz.json", this.state.right)}}>
+                <i className="material-icons">cloud_download</i>Download
+              </button>
             </div>
           </div>
        </div>
@@ -40,6 +59,11 @@ class App extends Component {
     moodleXMLtoJson(this.state.left, (res,err)=>{
       console.log(res,err)
       let right = JSON.stringify(res, null, 2)
+      if (err) {
+        alert("Not a valid MoodleXML file");
+        this.setState({right: ''});
+        return;
+      }
       this.setState({right});
     })
     
