27c64fbef98cbc01cd3964d9dd6b564b117585e2
sonsoleslp <sonsoleslp@hotmail.com>
2018-12-07 13:29:19 +0100

    Web UI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import moodleXMLtoJson from 'moodlexml-to-json';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      left: "",
+      right :""
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <header><h1>MoodleXML to JSON</h1></header>
+       <div className="content"> 
+          <div className="content-col left">
+            <h2>From</h2>
+            <textarea onChange={(e)=>{this.onWrite(e,'left')}} value={this.state.left}></textarea>
+            <div className="buttons">
+              <button onClick={this.convert.bind(this)}>Convert</button>
+            </div>
+          </div>
+          <div className="content-col right">
+            <h2>To</h2>
+            <textarea ref="right" onChange={(e)=>{this.onWrite(e,'right')}} value={this.state.right}></textarea>
+            <div className="buttons">
+              <button onClick={()=>{
+                this.refs.right.select();
+                document.execCommand('copy');
+              }}>Copy</button>
+              <button onClick={()=>{this.download("quiz.json",this.state.right)}}>Download</button>
+            </div>
+          </div>
+       </div>
       </div>
     );
   }
+  convert() {
+    moodleXMLtoJson(this.state.left, (res,err)=>{
+      console.log(res,err)
+      let right = JSON.stringify(res, null, 2)
+      this.setState({right});
+    })
+    
+  }
+  onWrite(e,side){
+    this.setState({ [side]: e.target.value});
+  }
+
+  download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  }
+
+
 }
 
 export default App;
