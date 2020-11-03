0508c11849e2d65e60ddc1de559d5f491f279286
Vagner Bezerra <vbezerra.ti@gmail.com>
2020-11-03 02:55:18 -0400

    Updated "sampleAiken.js".

--- src/sampleAiken.js
+++ src/sampleAiken.js
@@ -4,8 +4,9 @@ B. At the request of the person who submitted the Change request
 C. In case of emergency changes
 D. If another incident of the same type occurs again after a Change has been made
 Answer: A
+Name: Question 1
+Tag: list0,list1
 Feedback: Obviously
-
 
 multichoice
 2. When an organization decides to control the flow of incident information within the IT organization, 
@@ -15,32 +16,45 @@ B. Change Management
 C. Incident Management
 D. Problem Management
 Answer: C, D
+Name: Question 2
+Tag: list0,list1
 gfeed. Duh!
 
 essay
 3. The new role of social blogging in e-learning.
+Name: Question 3
 gfeed. Write something about Twitter, Facebook from the aspects of teaching and colloboration.
 
 shortanswer
 4. Calculate: 2 + 2 
 Answer: 4, four
+Name: Question 4
+Tag: list1,list2
 
 truefalse
 5. The founder of "Apple" was Steve Jobs.
 Answer: True
+Name: Question 5
+Tag: list1,list2
 Feedback: Steve Jobs is the CEO of Apple, which he co-founded in 1976.
 
 description
 6. Open Office is the alternative to Microsoft Office.
+Name: Question 6
+Tag: list3
 
 cloze
 7. Infrastructure <{1:MULTICHOICE:=Monitoring~Controlling~Service} will provide support teams with alerts directly allowing for faster resolution. 
 Such alerts do not need to be recorded in the Incident Management tool as there is little added value in this {1:MULTICHOICE:=true~false}. 
 Typically the incident will be resolved automatically before the customer recognises it.
+Name: Question 7
+Tag: list3
 
 numerical
 8. How many books are in ITIL V3? Answer only with a number.
 Answer: 5, 7
+Name: Question 8
+Tag: list4,list5
 
 order
 10. Place in ascending order
@@ -48,6 +62,8 @@ order
 2. 500
 3. 100
 ANSWER: 3,1,2
+Name: Question 10
+Tag: list4,list5
 
 matching
 11. Match cities and countries
@@ -57,6 +73,8 @@ match: Russia
 match: Finland
 3. Harbin
 match: China
-Feedback: Good job!`;
+Feedback: Good job!
+Name: Question 11
+Tag: list6`;
 
 export default aiken;
